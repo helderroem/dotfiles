58ccba18e911892cf9c9726c957b375cd6d8b6ab
Robin Marr <robinemarr@gmail.com>
2015-02-01 14:33:18 +0000

    Add sublime setup script

---
+++ sublime.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#install Package Control
+echo hello
+touch "./sublime-config/Package Control.sublime-package"
+curl "https://packagecontrol.io/Package%20Control.sublime-package" > "./sublime-config/Package Control.sublime-package"
